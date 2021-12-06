ec418e8f8b851376d5709b07bd030821d1613eb6
jason-xiao <t105360046@ntut.org.tw>
2021-12-06 15:02:20 +0800

    <app> impl JPCCTest read 4dtfrs dataset

--- app/JPCCTest/JPCCTest.cpp
+++ app/JPCCTest/JPCCTest.cpp
@@ -1,39 +1,74 @@
 #include <chrono>
+#include <execution>
 #include <filesystem>
 #include <iostream>
 #include <vector>
 
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
-#include <pcl/console/parse.h>
+#include <jpcc/common/Common.h>
+#include <jpcc/common/ParameterParser.h>
+#include <jpcc/io/DatasetParameter.h>
+#include <jpcc/io/PcapReaderParameter.h>
+#include <jpcc/io/PcapReader.h>
 
-#include <VelodyneCapture.h>
+#include <PCCChrono.h>
+#include <PCCMemory.h>
+
+using namespace pcc;
+using namespace jpcc;
+using namespace jpcc::common;
+using namespace jpcc::io;
+
+void test(const DatasetParameter&         datasetParameter,
+          const PcapReaderParameter&      pcapReaderParameter,
+          pcc::chrono::StopwatchUserTime& clock) {
+  PcapReader                pcapReader(datasetParameter, pcapReaderParameter);
+  std::vector<GroupOfFrame> sources;
+  size_t                    groupOfFrameSize = 32;
+  size_t                    startFrameIndex  = 0;
+  while (true) {
+    clock.start();
+    pcapReader.loadAll(sources, startFrameIndex, groupOfFrameSize, false);
+    clock.stop();
+    if (std::all_of(sources.begin(), sources.end(), [&](auto& frames) { return frames.size() < groupOfFrameSize; })) {
+      break;
+    }
+    startFrameIndex += groupOfFrameSize;
+  }
+}
 
 int main(int argc, char* argv[]) {
   std::cout << "JPCC Test App Start" << std::endl;
-  {
-    pcl::PointCloud<pcl::PointXYZ> cloud;
 
-    cloud.points.push_back(pcl::PointXYZ(1.0, 1.0, 1.0));
-    std::cout << "cloud.size=" << cloud.size() << std::endl;
-  }
-  {
-    std::string pcapPath;
-    pcl::console::parse_argument(argc, argv, "--pcapPath", pcapPath);
-    std::cout << "pcapPath=" << pcapPath << std::endl;
+  DatasetParameter    datasetParameter;
+  PcapReaderParameter pcapReaderParameter;
+  try {
+    ParameterParser pp;
+    pp.add(datasetParameter);
+    pp.add(pcapReaderParameter);
+    pp.parse(argc, argv);
+    std::cout << datasetParameter << std::endl;
+    std::cout << pcapReaderParameter << std::endl;
+  } catch (std::exception& e) { std::cerr << e.what() << std::endl; }
 
-    if (std::filesystem::exists(pcapPath)) {
-      velodyne::VLP16Capture capture(pcapPath);
-      int                    frameIndex = 0;
-      while (capture.isRun()) {
-        std::vector<velodyne::Laser> lasers;
-        if (capture.getQueueSize() > 0) {
-          capture.retrieve(lasers);
-          std::cout << frameIndex++ << " lasers.size()=" << lasers.size() << std::endl;
-        }
-      }
-    }
-  }
+  try {
+    ParameterParser pp;
+    // Timers to count elapsed wall/user time
+    pcc::chrono::Stopwatch<std::chrono::steady_clock> clockWall;
+    pcc::chrono::StopwatchUserTime                    clockUser;
+
+    clockWall.start();
+    test(datasetParameter, pcapReaderParameter, clockUser);
+    clockWall.stop();
+
+    auto totalWall      = std::chrono::duration_cast<std::chrono::milliseconds>(clockWall.count()).count();
+    auto totalUserSelf  = std::chrono::duration_cast<std::chrono::milliseconds>(clockUser.self.count()).count();
+    auto totalUserChild = std::chrono::duration_cast<std::chrono::milliseconds>(clockUser.children.count()).count();
+    std::cout << "Processing time (wall): " << totalWall / 1000.0 << " s\n";
+    std::cout << "Processing time (user.self): " << totalUserSelf / 1000.0 << " s\n";
+    std::cout << "Processing time (user.children): " << totalUserChild / 1000.0 << " s\n";
+    std::cout << "Peak memory: " << getPeakMemory() << " KB\n";
+  } catch (std::exception& e) { std::cerr << e.what() << std::endl; }
+
   std::cout << "JPCC Test App End" << std::endl;
   return 0;
 }
