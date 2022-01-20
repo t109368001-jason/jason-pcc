52d8bbf39c29047bd280a96c79456023c9b070d0
jason-xiao <t105360046@ntut.org.tw>
2022-01-20 16:30:51 +0800

    <build> change test branch to develop

--- scripts/docker-build-lab107.sh
+++ scripts/docker-build-lab107.sh
@@ -3,9 +3,11 @@ set -x
 
 REMOTE_HOST=lab107
 REMOTE_WORKING_DIR="~/git/jason-pcc"
+REMOTE_REPOSITORY_BRANCH=develop
 
 ssh -t ${REMOTE_HOST} "
     cd ${REMOTE_WORKING_DIR} &&
     git reset --hard &&
+    git checkout ${REMOTE_REPOSITORY_BRANCH}
     git pull &&
     bash scripts/docker-build.sh"
--- scripts/docker-test-lab107.sh
+++ scripts/docker-test-lab107.sh
@@ -2,10 +2,12 @@
 set -x
 
 REMOTE_HOST=lab107
-REMOTE_WORKING_DIR="~/t109368001/git/jason-pcc"
+REMOTE_WORKING_DIR="~/git/jason-pcc"
+REMOTE_REPOSITORY_BRANCH=develop
 
 ssh -t ${REMOTE_HOST} "
     cd ${REMOTE_WORKING_DIR} &&
     git reset --hard &&
+    git checkout ${REMOTE_REPOSITORY_BRANCH}
     git pull &&
     bash scripts/docker-test.sh"
