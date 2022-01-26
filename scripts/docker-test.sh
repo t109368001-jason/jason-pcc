16a7aac3bbb30383c58fc6f5caa64f34d4aa230a
jason-xiao <t105360046@ntut.org.tw>
2022-01-26 17:31:11 +0800

    <script> remove unused

--- scripts/docker-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-DOCKER_FILE_ARG="--file docker/test.yml"
-
-docker-compose ${DOCKER_FILE_ARG} down && \
-    docker-compose ${DOCKER_FILE_ARG} build && \
-    docker-compose ${DOCKER_FILE_ARG} up
