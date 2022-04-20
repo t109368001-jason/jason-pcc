c91f85f188b6f50772c535a82af1d85dcae3d36a
jason-xiao <jaSon6787forgItlab>
2022-04-20 15:13:23 +0800

    <feat> add OctreePointCloudOperation

---
+++ libs/process/include/jpcc/process/OctreePointCloudOperation.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#define PCL_NO_PRECOMPILE
+#include <pcl/octree/octree_pointcloud.h>
+
+#include <jpcc/common/Common.h>
+#include <jpcc/common/GroupOfFrame.h>
+#include <jpcc/octree/OctreeNBufBase.h>
+
+namespace jpcc::process {
+
+template <typename PointT = Point, bool onlyOnePointInVoxel = false>
+class OctreePointCloudOperation
+    : public pcl::octree::OctreePointCloud<
+          PointT,
+          typename std::conditional<onlyOnePointInVoxel,
+                                    pcl::octree::OctreeContainerPointIndex,
+                                    pcl::octree::OctreeContainerPointIndices>::type,
+          pcl::octree::OctreeContainerEmpty,
+          octree::OctreeNBufBase<2,
+                                 typename std::conditional<onlyOnePointInVoxel,
+                                                           pcl::octree::OctreeContainerPointIndex,
+                                                           pcl::octree::OctreeContainerPointIndices>::type,
+                                 pcl::octree::OctreeContainerEmpty>> {
+ public:
+  using Ptr          = shared_ptr<OctreePointCloudOperation>;
+  using Frame        = jpcc::Frame<PointT>;
+  using FramePtr     = typename Frame::Ptr;
+  using GroupOfFrame = jpcc::GroupOfFrame<PointT>;
+  using OctreeNBufBaseT =
+      octree::OctreeNBufBase<2,
+                             typename std::conditional<onlyOnePointInVoxel,
+                                                       pcl::octree::OctreeContainerPointIndex,
+                                                       pcl::octree::OctreeContainerPointIndices>::type,
+                             pcl::octree::OctreeContainerEmpty>;
+  using OctreePointCloudT =
+      pcl::octree::OctreePointCloud<PointT,
+                                    typename std::conditional<onlyOnePointInVoxel,
+                                                              pcl::octree::OctreeContainerPointIndex,
+                                                              pcl::octree::OctreeContainerPointIndices>::type,
+                                    pcl::octree::OctreeContainerEmpty,
+                                    OctreeNBufBaseT>;
+
+ protected:
+  FramePtr source_;
+  FramePtr target_;
+
+ public:
+  OctreePointCloudOperation(double resolution);
+
+  void setSource(FramePtr source);
+
+  void setTarget(FramePtr target);
+
+  FramePtr targetAndNotSource();
+};
+
+}  // namespace jpcc::process
+
+#include <jpcc/process/impl/OctreePointCloudOperation.hpp>
---
+++ libs/process/include/jpcc/process/impl/OctreePointCloudOperation.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "jpcc/process/OctreePointCloudOperation.h"
+
+namespace jpcc::process {
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT, bool quantize>
+OctreePointCloudOperation<PointT, quantize>::OctreePointCloudOperation(double resolution) :
+    OctreePointCloud(resolution) {}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT, bool quantize>
+void OctreePointCloudOperation<PointT, quantize>::setSource(FramePtr source) {
+  source_ = source;
+  this->switchBuffers(0);
+  this->deleteBuffer(0);
+  this->setInputCloud(source_);
+  this->addPointsFromInputCloud();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT, bool quantize>
+void OctreePointCloudOperation<PointT, quantize>::setTarget(FramePtr target) {
+  target_ = target;
+  this->switchBuffers(1);
+  this->deleteBuffer(1);
+  this->setInputCloud(target_);
+  this->addPointsFromInputCloud();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+template <typename PointT, bool quantize>
+FramePtr<PointT> OctreePointCloudOperation<PointT, quantize>::targetAndNotSource() {
+  auto indices = make_shared<Indices>();
+  this->switchBuffers(1);
+  this->getIndicesByFilter(
+      [](const typename OctreeNBufBaseT::BufferPattern& bufferPattern) { return !bufferPattern.test(0); }, *indices);
+
+  auto output = make_shared<Frame>();
+
+  process::split<PointT>(target_, indices, output, nullptr);
+  return output;
+}
+
+}  // namespace jpcc::process
