c7ddef3abbe0f26102332de42aa8fd5cf02f8c20
jason-xiao <t105360046@ntut.org.tw>
2022-06-22 17:10:00 +0800

    <scripts> fix scripts

--- scripts/app/JPCCAppAnalyzer[CR-R-100000][CR-Z-5000][ROR-R-500][ROR-N-1].sh
+++ scripts/app/JPCCAppAnalyzer[CR-R-100000][CR-Z-5000][ROR-R-500][ROR-N-1].sh
@@ -3,13 +3,13 @@ set -ex
 
 mkdir -p ./logs
 
-OUTPUT_DIR = ../../result/jason-pcc/analyzer[CR-R-100000][CR-Z-5000][ROR-R-500][ROR-N-1]/
+OUTPUT_DIR=../../result/jason-pcc/analyzer[CR-R-100000][CR-Z-5000][ROR-R-500][ROR-N-1]/
 
 ./bin/JPCCAppAnalyzer \
-  --conditions "r < 100000" \
-  --conditions "z < 5000" \
-  --radius 500 \
-  --minNeighborsInRadius 1 \
+  --preProcess.jpccConditionalRemoval.conditions "r < 100000" \
+  --preProcess.jpccConditionalRemoval.conditions "z < 5000" \
+  --preProcess.radiusOutlierRemoval.radius 500 \
+  --preProcess.radiusOutlierRemoval.minNeighborsInRadius 1 \
   --outputDir ${OUTPUT_DIR} \
   --configs ./cfg/app/Analyzer/ctc-raw.cfg |&
   tee ${OUTPUT_DIR}/JPCCAppAnalyzer.log
--- scripts/app/JPCCAppAnalyzer[CR-R-100000][ROR-R-500][ROR-N-1].sh
+++ scripts/app/JPCCAppAnalyzer[CR-R-100000][ROR-R-500][ROR-N-1].sh
@@ -3,12 +3,12 @@ set -ex
 
 mkdir -p ./logs
 
-OUTPUT_DIR = ../../result/jason-pcc/analyzer[CR-R-100000][ROR-R-500][ROR-N-1]/
+OUTPUT_DIR=../../result/jason-pcc/analyzer[CR-R-100000][ROR-R-500][ROR-N-1]/
 
 ./bin/JPCCAppAnalyzer \
-  --conditions "r < 100000" \
-  --radius 500 \
-  --minNeighborsInRadius 1 \
+  --preProcess.jpccConditionalRemoval.conditions "r < 100000" \
+  --preProcess.radiusOutlierRemoval.radius 500 \
+  --preProcess.radiusOutlierRemoval.minNeighborsInRadius 1 \
   --outputDir ${OUTPUT_DIR} \
   --configs ./cfg/app/Analyzer/ctc-raw.cfg |&
   tee ${OUTPUT_DIR}/JPCCAppAnalyzer.log
