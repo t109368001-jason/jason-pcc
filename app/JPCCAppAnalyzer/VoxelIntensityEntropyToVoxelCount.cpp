7ca5535c14ccb93974e9035b84adc06b6b2cc424
jason-xiao <t105360046@ntut.org.tw>
2022-06-29 11:13:51 +0800

    <feat> add VoxelIntensityEntropyToVoxelCount

---
+++ app/JPCCAppAnalyzer/VoxelIntensityEntropyToVoxelCount.cpp
@@ -0,0 +1,63 @@
+#include "VoxelIntensityEntropyToVoxelCount.h"
+
+#include <cmath>
+#include <map>
+
+#include <Eigen/Dense>
+
+#include <jpcc/common/Math.h>
+
+using namespace std;
+using namespace std::filesystem;
+using namespace jpcc::octree;
+
+namespace jpcc {
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+VoxelIntensityEntropyToVoxelCount::VoxelIntensityEntropyToVoxelCount(const float&       frequency,
+                                                                     const double&      resolution,
+                                                                     const std::string& outputDir) :
+    Analyzer(frequency, resolution, outputDir, "VoxelIntensityEntropyToVoxelCount"), octree_(resolution) {}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+void VoxelIntensityEntropyToVoxelCount::compute(FrameConstPtr background, FrameConstPtr dynamic, FrameConstPtr other) {
+  octree_.addFrame(0, background);
+  octree_.addFrame(1, dynamic);
+  octree_.addFrame(2, other);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+void VoxelIntensityEntropyToVoxelCount::finalCompute() {
+  map<int, array<size_t, BUFFER_SIZE>> countMap;
+  for (BufferIndex bufferIndex = 0; bufferIndex < BUFFER_SIZE; bufferIndex++) {
+    octree_.switchBuffers(bufferIndex);
+    for (auto it = octree_.leaf_depth_begin(), end = octree_.leaf_depth_end(); it != end; ++it) {
+      const vector<float>& intensities = it.getLeafContainer().getIntensities();
+      if (intensities.empty()) { continue; }
+
+      double intensityEntropy = entropy(intensities, 0.0f, 255.0f, 10.0f);
+
+      auto quantizedIntensityEntropy = (int)round(intensityEntropy * 10.0);
+
+      countMap.try_emplace(quantizedIntensityEntropy, array<size_t, BUFFER_SIZE>{0, 0, 0});
+      countMap.at(quantizedIntensityEntropy).at(bufferIndex) =
+          countMap.at(quantizedIntensityEntropy).at(bufferIndex) + 1;
+    }
+  }
+
+  ofstream ofs(filepath_);
+  ofs << "Voxel Intensity Entropy"
+      << ","
+      << "Voxel Count (Background)"
+      << ","
+      << "Voxel Count (Dynamic)"
+      << ","
+      << "Voxel Count (Other)" << endl;
+  for (const auto& [quantizedIntensityEntropy, countArray] : countMap) {
+    ofs << to_string(quantizedIntensityEntropy) << "," << countArray.at(0) << "," << countArray.at(1) << ","
+        << countArray.at(2) << endl;
+  }
+  octree_.deleteTree();
+}
+
+}  // namespace jpcc
---
+++ app/JPCCAppAnalyzer/VoxelIntensityEntropyToVoxelCount.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+
+#include <jpcc/common/Common.h>
+#include <jpcc/octree/JPCCOctreePointCloud.h>
+#include <jpcc/octree/OctreeContainerIntensities.h>
+
+#include "Analyzer.h"
+
+namespace jpcc {
+
+class VoxelIntensityEntropyToVoxelCount : public Analyzer {
+ public:
+  static constexpr octree::BufferIndex BUFFER_SIZE = 3;
+
+  using OctreeT = octree::JPCCOctreePointCloud<
+      PointXYZINormal,
+      octree::OctreeContainerIntensities,
+      pcl::octree::OctreeContainerEmpty,
+      octree::OctreeNBuf<BUFFER_SIZE, octree::OctreeContainerIntensities, pcl::octree::OctreeContainerEmpty>>;
+
+ protected:
+  OctreeT octree_;
+
+ public:
+  VoxelIntensityEntropyToVoxelCount(const float& frequency, const double& resolution, const std::string& outputDir);
+
+  void compute(FrameConstPtr background, FrameConstPtr dynamic, FrameConstPtr other) override;
+
+  void finalCompute() override;
+};
+
+}  // namespace jpcc
