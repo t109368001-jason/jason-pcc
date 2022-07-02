c186c3556d73489286bf41e93f9c3827bed1ebcd
jason-xiao <t105360046@ntut.org.tw>
2022-07-02 23:47:52 +0800

    <script> update scripts

--- scripts/app/JPCCAppAnalyzer-all.sh
+++ scripts/app/JPCCAppAnalyzer-all.sh
@@ -2,6 +2,4 @@
 set -ex
 
 bash ./scripts/app/JPCCAppAnalyzer.sh &
-bash ./scripts/app/JPCCAppAnalyzer[ROR-N-5].sh &
-bash ./scripts/app/JPCCAppAnalyzer[CR-R-100000][CR-Z-5000].sh &
-bash ./scripts/app/JPCCAppAnalyzer[CR-R-100000][CR-Z-5000][ROR-N-5].sh &
+bash ./scripts/app/JPCCAppAnalyzer[ROR-N-10].sh &
--- scripts/app/JPCCAppAnalyzer[ROR-N-10].sh
+++ scripts/app/JPCCAppAnalyzer[ROR-N-10].sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -ex
 
-OUTPUT_DIR=../../result/jason-pcc/analyzer[CR-R-100000][CR-Z-5000]/
+OUTPUT_DIR=../../result/jason-pcc/analyzer[ROR-N-10]/
 
 mkdir -p ${OUTPUT_DIR}
 
 ./bin/JPCCAppAnalyzer \
-  --preProcess.jpccConditionalRemoval.conditions "r < 100000" \
-  --preProcess.jpccConditionalRemoval.conditions "z < 5000" \
+  --preProcess.radiusOutlierRemoval.minNeighborsInRadius 10 \
   --app.outputDir ${OUTPUT_DIR} \
   --configs ./cfg/app/Analyzer/ctc-raw.cfg |& tee ${OUTPUT_DIR}JPCCAppAnalyzer-$(date +%Y%m%d-%H%M%S).log
