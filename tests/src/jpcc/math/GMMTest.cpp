8c7b1659c36979619efa6b6b7d681fa52ecd9c6f
jason-xiao <t105360046@ntut.org.tw>
2022-07-14 15:49:44 +0800

    <fix> fix test

--- tests/src/jpcc/math/GMMTest.cpp
+++ tests/src/jpcc/math/GMMTest.cpp
@@ -17,7 +17,7 @@ TEST(GMMTest, new_by_3_samples) {
   vector<GMM::SampleT> alternateCentroids;
 
   // when
-  const GMM gmm(samples, K, 0.05, alternateCentroids);
+  const GMM gmm(samples, K, 0.05, 0, alternateCentroids);
 
   // then
   const vector<Cluster::Ptr>& clusters = gmm.getClusters();
@@ -33,20 +33,24 @@ TEST(GMMTest, new_by_3_unique_samples) {
   // given
   constexpr int        K = 3;
   vector<GMM::SampleT> samples;
+  vector<int>          counts;
   for (size_t k = 0; k < K; k++) {
     int count = (rand() % 10) + 1;
+    counts.push_back(count);
     for (size_t j = 0; j < count; j++) { samples.push_back((GMM::SampleT)k); }
   }
   vector<GMM::SampleT> alternateCentroids;
+  int                  totalCount = 0;
+  for (size_t k = 0; k < K; k++) { totalCount += counts.at(k); }
 
   // when
-  const GMM gmm(samples, K, 0.05, alternateCentroids);
+  const GMM gmm(samples, K, 0.05, 0, alternateCentroids);
 
   // then
   const vector<Cluster::Ptr>& clusters = gmm.getClusters();
   EXPECT_EQ(clusters.size(), K);
   for (size_t k = 0; k < K; k++) {
-    EXPECT_NEAR(clusters.at(k)->getWeight(), 1.0 / K, TOLERANCE);
+    EXPECT_NEAR(clusters.at(k)->getWeight(), (double)counts.at(k) / (double)totalCount, TOLERANCE);
     EXPECT_NEAR(clusters.at(k)->getMean(), k, TOLERANCE);
     EXPECT_NEAR(clusters.at(k)->getVariance(), 0, TOLERANCE);
   }
@@ -60,14 +64,14 @@ TEST(GMMTest, new_3_cluster_3_variance) {
   vector<GMM::SampleT> samples;
   for (size_t k = 0; k < K; k++) {
     for (size_t i = 0; i < 100; i++) {
-      samples.push_back(means.at(k) - variances.at(k));
-      samples.push_back(means.at(k) + variances.at(k));
+      samples.push_back(means.at(k) - sqrt(variances.at(k)));
+      samples.push_back(means.at(k) + sqrt(variances.at(k)));
     }
   }
   vector<GMM::SampleT> alternateCentroids;
 
   // when
-  const GMM gmm(samples, K, 0.05, alternateCentroids);
+  const GMM gmm(samples, K, 0.05, 0, alternateCentroids);
 
   // then
   const vector<Cluster::Ptr>& clusters = gmm.getClusters();
@@ -92,7 +96,7 @@ TEST(GMMTest, getOptimalModelIndex) {
     }
   }
   vector<GMM::SampleT> alternateCentroids;
-  const GMM            gmm(samples, K, 0.05, alternateCentroids);
+  const GMM            gmm(samples, K, 0.05, 0, alternateCentroids);
 
   // then
   for (size_t k = 0; k < K; k++) {
@@ -111,19 +115,18 @@ TEST(GMMTest, updateModel) {
   vector<GMM::SampleT> samples;
   for (size_t k = 0; k < K; k++) {
     for (size_t i = 0; i < 100; i++) {
-      samples.push_back(means.at(k) - variances.at(k));
-      samples.push_back(means.at(k) + variances.at(k));
+      samples.push_back(means.at(k) - sqrt(variances.at(k)));
+      samples.push_back(means.at(k) + sqrt(variances.at(k)));
     }
   }
   vector<GMM::SampleT> alternateCentroids;
-  GMM                  gmm(samples, K, 0.05, alternateCentroids);
+  GMM                  gmm(samples, K, 0.05, 0, alternateCentroids);
 
   // when
-  double               d = sqrt((newVariance0 * 2) - variances.at(0));
   vector<GMM::SampleT> newSamples;
   for (size_t i = 0; i < 10; i++) {
-    newSamples.push_back(means.at(0) - d);
-    newSamples.push_back(means.at(0) + d);
+    newSamples.push_back(means.at(0) - sqrt(newVariance0));
+    newSamples.push_back(means.at(0) + sqrt(newVariance0));
   }
   gmm.updateModel(newSamples);
 
@@ -131,14 +134,14 @@ TEST(GMMTest, updateModel) {
   const vector<Cluster::Ptr>& clusters = gmm.getClusters();
   EXPECT_EQ(clusters.size(), K);
   EXPECT_NEAR(clusters.at(0)->getWeight(), 0.7610093851, TOLERANCE);
-  EXPECT_NEAR(clusters.at(0)->getMean(), 1.000039272, TOLERANCE);
-  EXPECT_NEAR(clusters.at(0)->getVariance(), 1.018755706, TOLERANCE);
+  EXPECT_NEAR(clusters.at(0)->getMean(), 1.000249155528542, TOLERANCE);
+  EXPECT_NEAR(clusters.at(0)->getVariance(), 1.1445250926215722, TOLERANCE);
   EXPECT_NEAR(clusters.at(1)->getWeight(), 0.1194953075, TOLERANCE);
-  EXPECT_NEAR(clusters.at(1)->getMean(), 49.99997528, TOLERANCE);
-  EXPECT_NEAR(clusters.at(1)->getVariance(), 10.0000546, TOLERANCE);
+  EXPECT_NEAR(clusters.at(1)->getMean(), 50, TOLERANCE);
+  EXPECT_NEAR(clusters.at(1)->getVariance(), 9.9999972267542034, TOLERANCE);
   EXPECT_NEAR(clusters.at(2)->getWeight(), 0.1194953075, TOLERANCE);
   EXPECT_NEAR(clusters.at(2)->getMean(), 499.9999921, TOLERANCE);
-  EXPECT_NEAR(clusters.at(2)->getVariance(), 100.0000188, TOLERANCE);
+  EXPECT_NEAR(clusters.at(2)->getVariance(), 100, TOLERANCE);
 }
 
 }  // namespace jpcc::octree
