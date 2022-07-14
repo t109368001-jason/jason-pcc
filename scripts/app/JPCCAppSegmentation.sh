dd0f209300ec8322cdbef17338d2ae716c05d1b2
jason-xiao <t105360046@ntut.org.tw>
2022-07-14 22:01:26 +0800

    <script> add JPCCAppSegmentation.sh

---
+++ scripts/app/JPCCAppSegmentation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+OUTPUT_DIR=../../result/jason-pcc/JPCCAppSegmentation/
+
+mkdir -p ${OUTPUT_DIR}
+
+./bin/JPCCAppSegmentation \
+  --configs --configs cfg/app/Segmentation/ctc-raw.cfg |& tee ${OUTPUT_DIR}JPCCAppSegmentation-$(date +%Y%m%d-%H%M%S).log
