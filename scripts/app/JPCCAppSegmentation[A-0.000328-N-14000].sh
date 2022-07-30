1fae2baef52b43379a1a73b0b29938c5437918c9
jason-xiao <t105360046@ntut.org.tw>
2022-07-30 22:16:03 +0800

    <script> update script

---
+++ scripts/app/JPCCAppSegmentation[A-0.000328-N-14000].sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+OUTPUT_FOLDER_PREFIX=../../dataset/
+OUTPUT_FOLDER=converted/ZX-XS-20220707-ctc-gmm-segmentation[A-0.000328-N-14000]/
+
+mkdir -p ${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}
+
+./bin/JPCCAppSegmentation \
+  --jpccGMMSegmentationParameter.alpha 0.000328 \
+  --jpccGMMSegmentationParameter.nTrain 14000 \
+  --outputDataset.folder ${OUTPUT_FOLDER} \
+  --configs cfg/app/Segmentation/ctc-raw.cfg |& tee ${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}JPCCAppSegmentation-$(date +%Y%m%d-%H%M%S).log
