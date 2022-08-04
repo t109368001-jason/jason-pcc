2871fc61689b80ba0bcac7efca2ac583e55e77ab
jason-xiao <t105360046@ntut.org.tw>
2022-08-04 19:48:55 +0800

    <script> update script

--- scripts/app/JPCCAppSegmentation.sh
+++ scripts/app/JPCCAppSegmentation.sh
@@ -7,4 +7,5 @@ OUTPUT_FOLDER=converted-$(date +%Y%m%d)/ZX-XS-20220707-ctc-gmm-segmentation/
 mkdir -p ${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}
 
 ./bin/JPCCAppSegmentation \
+  --outputDataset.folder ${OUTPUT_FOLDER} \
   --configs cfg/app/Segmentation/ctc-raw.cfg |& tee ${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}JPCCAppSegmentation-$(date +%Y%m%d-%H%M%S).log
