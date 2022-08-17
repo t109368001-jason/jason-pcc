7f61b5870a132bde3c31fc607633bfa03c484e84
jason-xiao <t105360046@ntut.org.tw>
2022-08-17 15:43:55 +0800

    <refactor>

--- libs/metric/src/JPCCMetric.cpp
+++ libs/metric/src/JPCCMetric.cpp
@@ -1,5 +1,6 @@
 #include <jpcc/metric/JPCCMetric.h>
 
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
@@ -22,19 +23,25 @@ void JPCCMetric::addBytes(const std::string& name, const uint64_t bytes) { bytes
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 void JPCCMetric::show() const {
+  size_t maxKeyLength = 0;
+  for (const auto& [name, _] : pointsMap_) { maxKeyLength = max(maxKeyLength, name.size()); }
+  for (const auto& [name, _] : bytesMap_) { maxKeyLength = max(maxKeyLength, name.size()); }
+  for (const auto& [name, _] : clockMap_) { maxKeyLength = max(maxKeyLength, name.size()); }
   cout << endl;
   cout << "Metric:" << endl;
   cout << "  Points:" << endl;
   for (const auto& [name, points] : pointsMap_) {  //
-    cout << "    " << name << ": " << points << " points" << endl;
+    cout << "    " << std::setfill(' ') << std::setw(maxKeyLength) << name << left << ": " << points << " points"
+         << endl;
   }
   cout << "  Bytes:" << endl;
   for (const auto& [name, bytes] : bytesMap_) {  //
-    cout << "    " << name << ": " << bytes << " bytes" << endl;
+    cout << "    " << std::setfill(' ') << std::setw(maxKeyLength) << name << left << ": " << bytes << " bytes" << endl;
   }
   cout << "  Processing time:" << endl;
   for (const auto& [name, stopWatch] : clockMap_) {
-    cout << "    " << name << ": " << (float)stopWatch.count().count() / 1000000000.0 << " s" << endl;
+    cout << "    " << std::setfill(' ') << std::setw(maxKeyLength) << name << left << ": "
+         << (float)stopWatch.count().count() / 1000000000.0 << " s" << endl;
   }
   cout << "Peak memory: " << getPeakMemory() << " KB\n\n";
 }
