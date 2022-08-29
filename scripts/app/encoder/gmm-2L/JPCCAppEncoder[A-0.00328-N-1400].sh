882eb76b879209f0425be1c502958ec3c7db0b6f
jason-xiao <t105360046@ntut.org.tw>
2022-08-29 15:56:07 +0800

    <config> update configs

--- scripts/app/encoder/gmm-2L/JPCCAppEncoder[A-0.00328-N-1400].sh
+++ scripts/app/encoder/gmm-2L/JPCCAppEncoder[A-0.00328-N-1400].sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-OUTPUT_FOLDER_PREFIX="../../dataset/"
-OUTPUT_FOLDER="converted-$(date +%Y%m%d-%H)/ZX-XS-20220707-ctc-gmm-segmentation[A-0.00328-N-1400]/"
+OUTPUT_FOLDER_PREFIX="../../result/"
+OUTPUT_FOLDER="encoded-$(date +%Y%m%d-%H)/ZX-XS-20220707-ctc-gmm-segmentation[A-0.00328-N-1400]/"
 
 mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
 
@@ -12,4 +12,5 @@ mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
   --jpccGMMSegmentationParameter.alpha 0.000255 \
   --jpccGMMSegmentationParameter.nTrain 18090 \
   --outputDataset.folder "${OUTPUT_FOLDER}" \
+  --jpccMetricParameter.outputCSVFolder "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
   --configs cfg/app/Encoder/ctc-raw.cfg |& tee "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}JPCCAppEncoder-$(date +%Y%m%d-%H%M%S).log"
--- scripts/app/encoder/gmm-2L/JPCCAppEncoder[STGT-5.984134206].sh
+++ scripts/app/encoder/gmm-2L/JPCCAppEncoder[STGT-5.984134206].sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-OUTPUT_FOLDER_PREFIX="../../dataset/"
-OUTPUT_FOLDER="converted-$(date +%Y%m%d-%H)/ZX-XS-20220707-ctc-gmm-segmentation[STGT-5.984134206]/"
+OUTPUT_FOLDER_PREFIX="../../result/"
+OUTPUT_FOLDER="encoded-$(date +%Y%m%d-%H)/ZX-XS-20220707-ctc-gmm-segmentation[STGT-5.984134206]/"
 
 mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
 
@@ -10,4 +10,5 @@ mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
   --jpccGMMSegmentationParameter.nullStaticThreshold -5.984134206 \
   --jpccGMMSegmentationParameter.nullStaticThreshold -5.984134206 \
   --outputDataset.folder "${OUTPUT_FOLDER}" \
+  --jpccMetricParameter.outputCSVFolder "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
   --configs cfg/app/Encoder/ctc-raw.cfg |& tee "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}JPCCAppEncoder-$(date +%Y%m%d-%H%M%S).log"
