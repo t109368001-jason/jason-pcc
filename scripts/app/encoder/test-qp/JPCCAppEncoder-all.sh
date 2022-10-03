4a061bc326a7d4fec2240533c9f92d05d7084c34
jason-xiao <t105360046@ntut.org.tw>
2022-10-03 16:15:31 +0800

    <script> fix script

--- scripts/app/encoder/test-qp/JPCCAppEncoder-all.sh
+++ scripts/app/encoder/test-qp/JPCCAppEncoder-all.sh
@@ -6,11 +6,11 @@ bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg][tmc3-85].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg][tmc3-90].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg][tmc3-95].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg][tmc3-100].sh &
-bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-100].sh
-bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-200].sh
-bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-300].sh
-bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-400].sh
-bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-500].sh
+bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-100].sh &
+bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-200].sh &
+bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-300].sh &
+bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-400].sh &
+bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-500].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[tmc3-80].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[tmc3-85].sh &
 bash ./scripts/app/encoder/test-qp/JPCCAppEncoder[tmc3-90].sh &
