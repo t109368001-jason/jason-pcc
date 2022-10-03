1cd1ae77e7e93a3288e345cf4ae47cd9268b8052
jason-xiao <t105360046@ntut.org.tw>
2022-10-03 16:14:09 +0800

    <script> add scripts

---
+++ scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-300].sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+OUTPUT_FOLDER_PREFIX="../../result/"
+OUTPUT_FOLDER="encoded-$(date +%Y%m%d-%H)/ZX-XS-20220707[seg-split][resolution-100]/"
+
+mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
+
+./bin/JPCCAppEncoder \
+  --outputDataset.folder "${OUTPUT_FOLDER}" \
+  --jpccGMMSegmentationParameter.outputType dynamic-static \
+  --jpccGMMSegmentationParameter.resolution 100.0 \
+  --app.compressedDynamicStreamPath "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}output-dynamic.bin" \
+  --app.compressedStaticStreamPath "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}output-static.bin" \
+  --app.compressedStaticAddedStreamPath "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}output-staticAdded.bin" \
+  --app.compressedStaticRemovedStreamPath "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}output-staticRemoved.bin" \
+  --jpccMetricParameter.outputCSVFolder "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}" \
+  --configs cfg/app/Encoder/ctc-raw.cfg |& tee "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}JPCCAppEncoder-$(date +%Y%m%d-%H%M%S).log"
