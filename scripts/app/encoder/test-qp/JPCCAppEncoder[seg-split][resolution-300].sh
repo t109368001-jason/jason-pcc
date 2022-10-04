18d1c5f8bc0eda4426e7b0a69d55cba32de52a7d
jason-xiao <t105360046@ntut.org.tw>
2022-10-04 21:20:45 +0800

    <script> update script

--- scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-300].sh
+++ scripts/app/encoder/test-qp/JPCCAppEncoder[seg-split][resolution-300].sh
@@ -2,7 +2,7 @@
 set -ex
 
 OUTPUT_FOLDER_PREFIX="../../result/"
-OUTPUT_FOLDER="encoded-$(date +%Y%m%d-%H)/ZX-XS-20220707[seg-split][resolution-100]/"
+OUTPUT_FOLDER="encoded-$(date +%Y%m%d-%H)/ZX-XS-20220707[seg-split][resolution-300]/"
 
 mkdir -p "${OUTPUT_FOLDER_PREFIX}${OUTPUT_FOLDER}"
 
