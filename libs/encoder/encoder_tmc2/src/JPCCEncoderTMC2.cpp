de56d72c408b67d85262c5262dc01a3c95abba34
jason-xiao <t105360046@ntut.org.tw>
2022-12-29 16:30:36 +0800

    <feat> impl JPCCEncoderTMC2.convertToCoderType

--- libs/encoder/encoder_tmc2/src/JPCCEncoderTMC2.cpp
+++ libs/encoder/encoder_tmc2/src/JPCCEncoderTMC2.cpp
@@ -13,10 +13,7 @@ namespace jpcc::encoder {
 JPCCEncoderTMC2::JPCCEncoderTMC2(const JPCCEncoderTMC2Parameter parameter) : JPCCEncoder(), parameter_(parameter) {}
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-bool JPCCEncoderTMC2::isConvertToCoderTypeThreadSafe() {
-  // TODO
-  return true;
-}
+bool JPCCEncoderTMC2::isConvertToCoderTypeThreadSafe() { return true; }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 bool JPCCEncoderTMC2::isEncodeThreadSafe() {
@@ -26,7 +23,14 @@ bool JPCCEncoderTMC2::isEncodeThreadSafe() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 void JPCCEncoderTMC2::convertToCoderType(const FramePtr& frame, CoderFramePtr& coderFrame) {
-  // TODO
+  auto _coderFrame = std::make_shared<PCCPointSet3>();
+  coderFrame       = _coderFrame;
+  _coderFrame->resize(frame->getPointCount());
+  for (size_t i = 0; i < _coderFrame->getPointCount(); i++) {
+    auto point          = (*frame)[i];
+    using TMC2ValueType = std::remove_reference_t<std::remove_const_t<decltype(((PCCPoint3D*)nullptr)->x())>>;
+    _coderFrame->setPosition(i, PCCPoint3D((TMC2ValueType)point[0], (TMC2ValueType)point[1], (TMC2ValueType)point[2]));
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
