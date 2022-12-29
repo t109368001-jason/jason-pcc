c87fe052a8d96772fab71b2494db1ff34779b43a
jason-xiao <t105360046@ntut.org.tw>
2022-12-29 16:35:20 +0800

    <feat> impl JPCCDecoderTMC2.convertFromCoderType

--- libs/decoder/decoder_tmc2/src/JPCCDecoderTMC2.cpp
+++ libs/decoder/decoder_tmc2/src/JPCCDecoderTMC2.cpp
@@ -2,13 +2,14 @@
 
 #include <sstream>
 
+#include <PCCPointSet.h>
+
+using namespace pcc;
+
 namespace jpcc::decoder {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
-bool JPCCDecoderTMC2::isConvertFromCoderTypeThreadSafe() {
-  // TODO
-  return true;
-}
+bool JPCCDecoderTMC2::isConvertFromCoderTypeThreadSafe() { return true; }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 void JPCCDecoderTMC2::decode(std::istream& is, std::shared_ptr<void>& coderReconstructFrame) {
@@ -17,7 +18,15 @@ void JPCCDecoderTMC2::decode(std::istream& is, std::shared_ptr<void>& coderRecon
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 void JPCCDecoderTMC2::convertFromCoderType(const std::shared_ptr<void>& coderFrame, FramePtr& frame) {
-  // TODO
+  auto _coderFrame = std::static_pointer_cast<PCCPointSet3>(coderFrame);
+  frame->resize(_coderFrame->getPointCount());
+  for (size_t i = 0; i < frame->getPointCount(); i++) {
+    const auto& coderPoint = (*_coderFrame)[i];
+    auto&       point      = (*frame)[i];
+    point[0]               = coderPoint.x();
+    point[1]               = coderPoint.y();
+    point[2]               = coderPoint.z();
+  }
 }
 
 }  // namespace jpcc::decoder
