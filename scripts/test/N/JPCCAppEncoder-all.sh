d90f1b41820106e55c6bd6850fe1744ed31ec091
jason-xiao <t105360046@ntut.org.tw>
2023-03-01 22:20:05 +0800

    <script> update scripts

--- scripts/test/N/JPCCAppEncoder-all.sh
+++ scripts/test/N/JPCCAppEncoder-all.sh
@@ -5,6 +5,12 @@ OUTPUT_FOLDER_PREFIX=${OUTPUT_FOLDER_PREFIX:="../../result/test-N-$(date +%Y%m%d
 
 source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/JPCCAppEncoderTestN.sh"
 
+JPCCAppEncoderTestN 100 53 175 &
+JPCCAppEncoderTestN 200 60 175 &
+JPCCAppEncoderTestN 300 65 175 &
+JPCCAppEncoderTestN 400 68 175 &
+JPCCAppEncoderTestN 500 71 175 &
+
 JPCCAppEncoderTestN 100 53 350 &
 JPCCAppEncoderTestN 200 60 350 &
 JPCCAppEncoderTestN 300 65 350 &
@@ -28,3 +34,9 @@ JPCCAppEncoderTestN 200 60 2100 &
 JPCCAppEncoderTestN 300 65 2100 &
 JPCCAppEncoderTestN 400 68 2100 &
 JPCCAppEncoderTestN 500 71 2100 &
+
+JPCCAppEncoderTestN 100 53 2800 &
+JPCCAppEncoderTestN 200 60 2800 &
+JPCCAppEncoderTestN 300 65 2800 &
+JPCCAppEncoderTestN 400 68 2800 &
+JPCCAppEncoderTestN 500 71 2800 &
--- scripts/test/alpha/JPCCAppEncoder-all.sh
+++ scripts/test/alpha/JPCCAppEncoder-all.sh
@@ -5,14 +5,38 @@ OUTPUT_FOLDER_PREFIX=${OUTPUT_FOLDER_PREFIX:="../../result/test-alpha-$(date +%Y
 
 source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/JPCCAppEncoderTestAlpha.sh"
 
+JPCCAppEncoderTestAlpha 100 53 0.00099 &
+JPCCAppEncoderTestAlpha 200 60 0.00099 &
+JPCCAppEncoderTestAlpha 300 65 0.00099 &
+JPCCAppEncoderTestAlpha 400 68 0.00099 &
+JPCCAppEncoderTestAlpha 500 71 0.00099 &
+
 JPCCAppEncoderTestAlpha 100 53 0.00198 &
 JPCCAppEncoderTestAlpha 200 60 0.00198 &
 JPCCAppEncoderTestAlpha 300 65 0.00198 &
 JPCCAppEncoderTestAlpha 400 68 0.00198 &
 JPCCAppEncoderTestAlpha 500 71 0.00198 &
 
+JPCCAppEncoderTestAlpha 100 53 0.00328 &
+JPCCAppEncoderTestAlpha 200 60 0.00328 &
+JPCCAppEncoderTestAlpha 300 65 0.00328 &
+JPCCAppEncoderTestAlpha 400 68 0.00328 &
+JPCCAppEncoderTestAlpha 500 71 0.00328 &
+
+JPCCAppEncoderTestAlpha 100 53 0.00395 &
+JPCCAppEncoderTestAlpha 200 60 0.00395 &
+JPCCAppEncoderTestAlpha 300 65 0.00395 &
+JPCCAppEncoderTestAlpha 400 68 0.00395 &
+JPCCAppEncoderTestAlpha 500 71 0.00395 &
+
 JPCCAppEncoderTestAlpha 100 53 0.00656 &
 JPCCAppEncoderTestAlpha 200 60 0.00656 &
 JPCCAppEncoderTestAlpha 300 65 0.00656 &
 JPCCAppEncoderTestAlpha 400 68 0.00656 &
 JPCCAppEncoderTestAlpha 500 71 0.00656 &
+
+JPCCAppEncoderTestAlpha 100 53 0.01310 &
+JPCCAppEncoderTestAlpha 200 60 0.01310 &
+JPCCAppEncoderTestAlpha 300 65 0.01310 &
+JPCCAppEncoderTestAlpha 400 68 0.01310 &
+JPCCAppEncoderTestAlpha 500 71 0.01310 &
