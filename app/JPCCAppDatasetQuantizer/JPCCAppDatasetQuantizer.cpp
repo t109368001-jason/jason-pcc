4c920abff4d081c4e68050360d4892297134db8b
jason-xiao <t105360046@ntut.org.tw>
2023-05-15 15:50:03 +0800

    <update> remove duplicated points

--- app/JPCCAppDatasetQuantizer/JPCCAppDatasetQuantizer.cpp
+++ app/JPCCAppDatasetQuantizer/JPCCAppDatasetQuantizer.cpp
@@ -9,6 +9,7 @@
 #include <jpcc/common/ParameterParser.h>
 #include <jpcc/io/Reader.h>
 #include <jpcc/io/PlyIO.h>
+#include <pcl/octree/octree_pointcloud.h>
 
 #include "AppParameter.h"
 
@@ -29,36 +30,56 @@ void quantize(const AppParameter& parameter, Stopwatch& clock) {
     reader->loadAll(frameNumber, groupOfFramesSize, frames, parameter.parallel);
     clock.stop();
 
+    GroupOfFrame outputFrames(frames.size());
+
     auto func = [&](const FramePtr& frame) {
       if (!frame) {
-        return;
+        return FramePtr{};
       }
-      if (!parameter.offset.empty()) {
-        for (size_t i = 0; i < frame->getPointCount(); i++) {
-          PointType& point = (*frame)[i];
+      auto outputFrame = make_shared<Frame>();
+      outputFrame->addRemoveAttributes(*frame);
+
+      auto                                         cloud = make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+      pcl::octree::OctreePointCloud<pcl::PointXYZ> octree(parameter.qp);
+      octree.setInputCloud(cloud);
+      size_t oIndex = 0;
+      for (size_t i = 0; i < frame->getPointCount(); i++) {
+        PointType& point = (*frame)[i];
+        if (!parameter.offset.empty()) {
           point[0] += static_cast<PointValueType>(parameter.offset[0]);
           point[1] += static_cast<PointValueType>(parameter.offset[1]);
           point[2] += static_cast<PointValueType>(parameter.offset[2]);
         }
-      }
-      for (size_t i = 0; i < frame->getPointCount(); i++) {
-        PointType& point = (*frame)[i];
         point[0] /= parameter.qp;
         point[1] /= parameter.qp;
         point[2] /= parameter.qp;
         THROW_IF_NOT(point[0] >= 0 && point[1] >= 0 && point[1] >= 0);
+        const pcl::PointXYZ& p =
+            pcl::PointXYZ(static_cast<float>(point[0]), static_cast<float>(point[1]), static_cast<float>(point[2]));
+        if (!octree.isVoxelOccupiedAtPoint(p)) {
+          octree.addPointToCloud(p, cloud);
+          outputFrame->addPoint(point);
+          if (frame->hasReflectances()) {
+            outputFrame->setReflectance(oIndex, frame->getReflectance(i));
+          }
+          if (frame->hasNormals()) {
+            outputFrame->setNormal(oIndex, frame->getNormal(i));
+          }
+          oIndex++;
+        }
       }
+      return outputFrame;
     };
 
     if (!parameter.parallel) {
-      for (const auto& frame : frames) {
-        func(frame);
+      for (size_t i = 0; i < frames.size(); i++) {
+        outputFrames[i] = func(frames[i]);
       }
     } else {
       const auto range = boost::counting_range<size_t>(0, frames.size());
       std::for_each(std::execution::par, range.begin(), range.end(),
                     [&](const size_t& i) {  //
-                      func(frames[i]);
+                      outputFrames[i] = func(frames[i]);
                     });
     }
 
