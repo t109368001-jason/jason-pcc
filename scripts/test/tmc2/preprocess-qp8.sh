fc9cfadcfc7062ae62d4bc1cbe66055084dcbeb3
jason-xiao <t105360046@ntut.org.tw>
2023-05-17 19:04:16 +0800

    <script> fix script

--- scripts/test/tmc2/preprocess-qp8.sh
+++ scripts/test/tmc2/preprocess-qp8.sh
@@ -9,12 +9,12 @@ function PccAppEncoderTest() {
   mkdir -p "../../result/${output_folder}/"
 
   ./bin/PccAppEncoder \
-    ${@:-3} \
-    --config "./3rdparty/mpeg-pcc-tmc2/cfg/common/ctc-common.cfg" \
-    --config "./3rdparty/mpeg-pcc-tmc2/cfg/sequence/ZX-XS-20220707-preprocess-qp8.cfg" \
-    --config "./3rdparty/mpeg-pcc-tmc2/cfg/condition/ctc-${condition}.cfg" \
-    --config "./3rdparty/mpeg-pcc-tmc2/cfg/rate/ctc-${rate}.cfg" \
-    --reconstructedDataPath "../../result/${output_folder}/rec_%05d.ply" \
-    --compressedStreamPath  "../../result/${output_folder}/output.bin" \
-    --configurationFolder ./3rdparty/mpeg-pcc-tmc2/cfg/ |& tee "../../result/${output_folder}/PccAppEncoder-$(date +%Y%m%d-%H%M%S).log"
+    ${@:3} \
+    --config="./3rdparty/mpeg-pcc-tmc2/cfg/common/ctc-common.cfg" \
+    --config="./3rdparty/mpeg-pcc-tmc2/cfg/sequence/ZX-XS-20220707-preprocess-qp8.cfg" \
+    --config="./3rdparty/mpeg-pcc-tmc2/cfg/condition/ctc-${condition}.cfg" \
+    --config="./3rdparty/mpeg-pcc-tmc2/cfg/rate/ctc-${rate}.cfg" \
+    --reconstructedDataPath="../../result/${output_folder}/rec_%05d.ply" \
+    --compressedStreamPath="../../result/${output_folder}/output.bin" \
+    --configurationFolder=./3rdparty/mpeg-pcc-tmc2/cfg/ |& tee "../../result/${output_folder}/PccAppEncoder-$(date +%Y%m%d-%H%M%S).log"
 }
